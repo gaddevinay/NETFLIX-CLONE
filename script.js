96f9c5e8dfe935275998fb2c23a73cc071bb8af7
Vinay Gadde <128584849+vinayhack@users.noreply.github.com>
2023-08-07 20:02:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,193 @@
+
+function openanswer1() {
+    let plus1 = document.getElementById("plus1");
+    let answer1 = document.getElementById("answer1");
+    let plus2 = document.getElementById("plus2");
+    let answer2 = document.getElementById("answer2");
+    let plus3 = document.getElementById("plus3");
+    let answer3 = document.getElementById("answer3");
+    let plus4 = document.getElementById("plus4");
+    let answer4 = document.getElementById("answer4");
+    let plus5 = document.getElementById("plus5");
+    let answer5 = document.getElementById("answer5");
+    let plus6 = document.getElementById("plus6");
+    let answer6 = document.getElementById("answer6");
+    if (answer1.style.display == "none") {
+        answer5.style.display = "none";
+        answer2.style.display = "none";
+        answer3.style.display = "none";
+        answer4.style.display = "none";
+        answer6.style.display = "none";
+        plus5.style.transform = 'rotate(0deg)';
+        plus2.style.transform = 'rotate(0deg)';
+        plus3.style.transform = 'rotate(0deg)';
+        plus4.style.transform = 'rotate(0deg)';
+        plus6.style.transform = 'rotate(0deg)';
+        answer1.style.display = "block";
+        plus1.style.transform = 'rotate(-45deg)';
+    }
+    else {
+        answer1.style.display = "none";
+        plus1.style.transform = 'rotate(0deg)';
+    }
+}
+function openanswer2() {
+    let plus1 = document.getElementById("plus1");
+    let answer1 = document.getElementById("answer1");
+    let plus2 = document.getElementById("plus2");
+    let answer2 = document.getElementById("answer2");
+    let plus3 = document.getElementById("plus3");
+    let answer3 = document.getElementById("answer3");
+    let plus4 = document.getElementById("plus4");
+    let answer4 = document.getElementById("answer4");
+    let plus5 = document.getElementById("plus5");
+    let answer5 = document.getElementById("answer5");
+    let plus6 = document.getElementById("plus6");
+    let answer6 = document.getElementById("answer6");
+    if (answer2.style.display == "none") {
+        answer5.style.display = "none";
+        answer1.style.display = "none";
+        answer3.style.display = "none";
+        answer4.style.display = "none";
+        answer6.style.display = "none";
+        plus5.style.transform = 'rotate(0deg)';
+        plus1.style.transform = 'rotate(0deg)';
+        plus3.style.transform = 'rotate(0deg)';
+        plus4.style.transform = 'rotate(0deg)';
+        plus6.style.transform = 'rotate(0deg)';
+        answer2.style.display = "block";
+        plus2.style.transform = 'rotate(-45deg)';
+    }
+    else {
+        answer2.style.display = "none";
+        plus2.style.transform = 'rotate(0deg)';
+    }
+}
+function openanswer3() {
+    let plus1 = document.getElementById("plus1");
+    let answer1 = document.getElementById("answer1");
+    let plus2 = document.getElementById("plus2");
+    let answer2 = document.getElementById("answer2");
+    let plus3 = document.getElementById("plus3");
+    let answer3 = document.getElementById("answer3");
+    let plus4 = document.getElementById("plus4");
+    let answer4 = document.getElementById("answer4");
+    let plus5 = document.getElementById("plus5");
+    let answer5 = document.getElementById("answer5");
+    let plus6 = document.getElementById("plus6");
+    let answer6 = document.getElementById("answer6");
+    if (answer3.style.display == "none") {
+        answer1.style.display = "none";
+        answer2.style.display = "none";
+        answer3.style.display = "none";
+        answer4.style.display = "none";
+        answer5.style.display = "none";
+        plus1.style.transform = 'rotate(0deg)';
+        plus2.style.transform = 'rotate(0deg)';
+        plus3.style.transform = 'rotate(0deg)';
+        plus4.style.transform = 'rotate(0deg)';
+        plus5.style.transform = 'rotate(0deg)';
+        answer3.style.display = "block";
+        plus3.style.transform = 'rotate(-45deg)';
+    }
+    else {
+        answer3.style.display = "none";
+        plus3.style.transform = 'rotate(0deg)';
+    }
+}
+function openanswer4() {
+    let plus1 = document.getElementById("plus1");
+    let answer1 = document.getElementById("answer1");
+    let plus2 = document.getElementById("plus2");
+    let answer2 = document.getElementById("answer2");
+    let plus3 = document.getElementById("plus3");
+    let answer3 = document.getElementById("answer3");
+    let plus4 = document.getElementById("plus4");
+    let answer4 = document.getElementById("answer4");
+    let plus5 = document.getElementById("plus5");
+    let answer5 = document.getElementById("answer5");
+    let plus6 = document.getElementById("plus6");
+    let answer6 = document.getElementById("answer6");
+    if (answer4.style.display == "none") {
+        answer1.style.display = "none";
+        answer2.style.display = "none";
+        answer3.style.display = "none";
+        answer6.style.display = "none";
+        answer5.style.display = "none";
+        plus1.style.transform = 'rotate(0deg)';
+        plus2.style.transform = 'rotate(0deg)';
+        plus3.style.transform = 'rotate(0deg)';
+        plus6.style.transform = 'rotate(0deg)';
+        plus5.style.transform = 'rotate(0deg)';
+        answer4.style.display = "block";
+        plus4.style.transform = 'rotate(-45deg)';
+    }
+    else {
+        answer4.style.display = "none";
+        plus4.style.transform = 'rotate(0deg)';
+    }
+}
+function openanswer5() {
+    let plus1 = document.getElementById("plus1");
+    let answer1 = document.getElementById("answer1");
+    let plus2 = document.getElementById("plus2");
+    let answer2 = document.getElementById("answer2");
+    let plus3 = document.getElementById("plus3");
+    let answer3 = document.getElementById("answer3");
+    let plus4 = document.getElementById("plus4");
+    let answer4 = document.getElementById("answer4");
+    let plus5 = document.getElementById("plus5");
+    let answer5 = document.getElementById("answer5");
+    let plus6 = document.getElementById("plus6");
+    let answer6 = document.getElementById("answer6");
+    if (answer5.style.display == "none") {
+        answer1.style.display = "none";
+        answer2.style.display = "none";
+        answer3.style.display = "none";
+        answer4.style.display = "none";
+        answer6.style.display = "none";
+        plus1.style.transform = 'rotate(0deg)';
+        plus2.style.transform = 'rotate(0deg)';
+        plus3.style.transform = 'rotate(0deg)';
+        plus4.style.transform = 'rotate(0deg)';
+        plus6.style.transform = 'rotate(0deg)';
+        answer5.style.display = "block";
+        plus5.style.transform = 'rotate(-45deg)';
+    }
+    else {
+        answer5.style.display = "none";
+        plus5.style.transform = 'rotate(0deg)';
+    }
+}
+function openanswer6() {
+    let plus1 = document.getElementById("plus1");
+    let answer1 = document.getElementById("answer1");
+    let plus2 = document.getElementById("plus2");
+    let answer2 = document.getElementById("answer2");
+    let plus3 = document.getElementById("plus3");
+    let answer3 = document.getElementById("answer3");
+    let plus4 = document.getElementById("plus4");
+    let answer4 = document.getElementById("answer4");
+    let plus5 = document.getElementById("plus5");
+    let answer5 = document.getElementById("answer5");
+    let plus6 = document.getElementById("plus6");
+    let answer6 = document.getElementById("answer6");
+    if (answer6.style.display == "none") {
+        answer1.style.display = "none";
+        answer2.style.display = "none";
+        answer3.style.display = "none";
+        answer4.style.display = "none";
+        answer5.style.display = "none";
+        plus1.style.transform = 'rotate(0deg)';
+        plus2.style.transform = 'rotate(0deg)';
+        plus3.style.transform = 'rotate(0deg)';
+        plus4.style.transform = 'rotate(0deg)';
+        plus5.style.transform = 'rotate(0deg)';
+        answer6.style.display = "block";
+        plus6.style.transform = 'rotate(-45deg)';
+    }
+    else {
+        answer6.style.display = "none";
+        plus6.style.transform = 'rotate(0deg)';
+    }
+}   
